26ee0d273a3b9e58fe549dc4760573aebc85aa6f
vtheno <a2550591@gmail.com>
2018-03-29 10:14:14 +0800

    changed Parsing

--- SimpleLex.py
+++ SimpleLex.py
@@ -27,14 +27,14 @@ class KeyWord:
     @property
     def symbols(self):
         NotImplementedError
-
 class functor_Lexical(Lexical):
     def __init__(self,keyword):
         self.KeyWord = keyword
         # Keyword : KeyWord        
         # fLexical : KeyWord -> Lexical
     def isGraph(self,c):
-        t = """! " # $ % & ' ( ) * + , - . / 0 1 2 3 4 5 6 7 8 9 : ; < = > ? @ A B C D E F G H I J K L M N O P Q R S T U V W X Y Z [ \ ] ^ _ ` a b c d e f g h i j k l m n o p q r s t u v w x y z { | } ~""".split()
+        #t = """! " # $ % & ' ( ) * + , - . / 0 1 2 3 4 5 6 7 8 9 : ; < = > ? @ A B C D E F G H I J K L M N O P Q R S T U V W X Y Z [ \ ] ^ _ ` a b c d e f g h i j k l m n o p q r s t u v w x y z { | } ~""".split()
+        t = ['!','"','#','$','%','&','\'','(',')','*','+',',','-','.','/','0','1','2','3','4','5','6','7','8','9',':',';','<','=','>','?','@','A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z','[','\\',']','^','_','`','a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z','{','|','}','~']
         return c in t
     def isPunct(self,c):
         t = ['!','"','#','$','%', '&',"'",
@@ -48,6 +48,7 @@ class functor_Lexical(Lexical):
     def symbolic (self,sy,ss):
         lsy = list(sy)
         lss = list(ss)
+        #print "symbolic:",repr(sy),repr(ss)
         if lss == []:
             return self.Key(sy),ss
         else:
@@ -55,23 +56,17 @@ class functor_Lexical(Lexical):
             if sy in self.KeyWord.symbols() or (not self.isPunct(c)):
                 #if member (sy,Keyword.symbols) orelse not (Char.isPunct c)
                 return self.Key(sy),ss
-            return self.symbolic (sy + c,ss1)#''.join(ss1))
+            else:
+                return self.symbolic (sy + c,ss1)
     def dropl(self,fn,ss):
-        right = [ ]
-        #i = 0
-        #while i < len(ss) and not fn(i):
-        #for i in ss :
-        #    if not fn(i):
-        #        result.append (i)
-        #return result
+        #right = [ ]
         i = 0
         while i < len(ss) and fn(ss[i]):
-            right .append (ss[i])
+            #right .append (ss[i])
             i+=1
         return ss[i:]
     def splitl(self,fn,ss):
         right = []
-        #fail = []
         i = 0 
         while i < len(ss) and fn(ss[i]):
             right .append (ss[i])
@@ -87,116 +82,92 @@ class functor_Lexical(Lexical):
             if c.isalnum() :
                 id,ss2 = self.splitl (lambda x:x.isalnum(),ss)
                 tok = self.alphaTok (''.join(id))
-                #print id,ss2,tok
-                return self.scaning ([tok] + toks,''.join(ss2))
+                #print "id,ss2,tok:",id,ss2,repr(tok)
+                return self.scaning ([tok] + toks,ss2)
             elif self.isPunct (c):
-                tok,ss2 = self.symbolic (str(c),''.join(ss1))
-                #print "tok,ss2:",tok,ss2
-                return self.scaning ([tok] + toks,''.join(ss2))
+                tok,ss2 = self.symbolic (str(c),ss1)
+                #print "tok,ss2:",tok,repr(ss2)
+                return self.scaning ([tok] + toks,ss2)
             else:
                 return self.scaning (toks,self.dropl ((lambda c:not self.isGraph(c)),
-                                                      ''.join(ss)))
+                                                      ss))
     def scan (self,a):
         return self.scaning ( [ ] ,a)
-#
-class LamKey(KeyWord):
-    @classmethod
-    def alphas(self):
-        return []
-    @classmethod
-    def symbols(self):
-        return ["(",")","'","->","'"]
-a = functor_Lexical (LamKey)
-#print a.alphaTok ("(")
-#print a.isPunct ("(")
-#print a.scan(" ( 'a -> 'b )")
-#print a.Token
+
 class SyntaxErrors(Exception): pass
 class Parsing:
     def __init__(self,Lex):
         self.Lex = Lex
-    def id(self,lst): # right
+    def id(self,lst): # right it's equal Parsec succeed or result or return
         if lst == [ ]:
             raise SyntaxErrors ("Identifier excepted")
         x,toks = lst[0],lst[1:]
         if isinstance(x,self.Lex.Id):
-            return (x.id,toks)
+            return [(x.id,toks)]
         else:
             raise SyntaxErrors ("Identifier excepted")
-    def _a(self,a):# right $a 0
-        def c_a(lst):
+    def key(self,a):# right $a 0 like id but it need type Lex.Key
+        def ckey(lst):
+            #print "key:",lst
             if lst == [ ]:
                 raise SyntaxErrors ("Identifier excepted")
             x,toks = lst[0],lst[1:]
             if isinstance(x,self.Lex.Key):
                 if a == x.key:
-                    return (a,toks)
+                    return [(a,toks)]
                 else:
                     raise SyntaxErrors(str(a))
             else:
                 raise SyntaxErrors ("Symbol excepted")
-        return c_a
-    def empty (self,toks):#right
+        return ckey
+    def empty (self,toks):#right it's Parsec fail or zero
         return ([ ],toks)
-    def Or(self,ph1,ph2):# right || 0
+    def Or(self,ph1,ph2):# right || 0 it's Parsec || or ++ or Alt or "Or"
         def curryOr(toks):
             try:
                 return ph1(toks)
             except SyntaxErrors , _:
                 return ph2(toks)
         return curryOr
-    def NN(self,ph): # !! 0
-        def curryNN(toks):
-            try :
-                return ph(toks)
-            except SyntaxErrors,msg:
-                raise Exception ("Syntax error:" + msg)
-        return curryNN
-    def seq(self,ph1,ph2): # right -- 5
-        def currySeq(toks):
-            x,toks2 = ph1 (toks)
-            y,toks3 = ph2 (toks2)
-            return ( (x,y) ,toks3)
-        return currySeq
-    def bind(self,ph,f): # right >> 3
-        def curryBind(toks):
-            x,toks2 = ph(toks)
-            #print "bind:",x,toks2,f
-            r = f(x)
-            return (r,toks2)
-        return curryBind
-    def _seq(self,a,ph): # right $-- 6 
-        return self.bind(self.seq(self._a(a),self.NN(ph)),
-                         (lambda x:x[1]))
-    def repeat(self,ph): #right
-        def curryR(toks):
-            left = self.bind(self.seq(ph,(self.repeat(ph)))
-                             ,(lambda a:[a[0]] + a[1]))
-            return self.Or(left,self.empty)(toks)
-        return curryR
-    def infixes(self,ph,prec_of,Apply):
-        def over(k):
-            def _o(toks):
-                return Next(k)(ph(toks))
-            return _o
-        def Next(k):
-            def _n(inp):
-                x,lst = inp
-                a,toks = lst[0],lst[1:]
-                if isinstance(a,Lex.Key):
-                    a = a.key
-                    if prec_of(a) < k:
-                        return (x,([Lex.Key(a)]+toks))
-                    return Next(k)( self.bind(over(prec_of(a)),Apply(a,x)))(toks)
-                else:
-                    return (x,lst)
-            return _n
-        return over(0)
+    def uncurry(self,f):
+        return lambda a,b : f (a)(b)
+    def concat(self,lst):
+        temp = []
+        for i in lst:
+            temp.extend(i)
+        return temp
+    def fmap(self,func,lst):
+        temp = []
+        while lst!=[]:
+            now = lst[0]
+            #print now,lst
+            try:
+                temp.append( func(*now) )
+            except Exception,e:
+                print e,type(e),e.args
+            finally:
+                lst = lst[1:]
+        else:
+            return temp
+    def bind(self,p,f):
+        def bind__(inp):
+            temp = p (inp)
+            return self.concat ( self.fmap (self.uncurry(f),temp))
+        return bind__
+    def mreturn(self,v):
+        def curryReturn(inp):
+            return [(v,inp)]
+        return curryReturn
+    def seq(self,p,q):
+        return self.bind(p,lambda v : self.bind(q,lambda w : self.mreturn((v,w))))
+    def many(self,p):
+        return self.Or(bind(p)(lambda x:bind(many(p))(lambda xs: mreturn ([x]+xs) )))(mreturn([]))
     def reader(self,ph): # right
         def cR(a):
             tmp = self.Lex.scan(a)
             value = ph (tmp)
-            x,xs = value
+            #print type(value),value
+            x,xs = value[0]
             #print "value:",value
             if xs == []:
                 return x
@@ -204,44 +175,88 @@ class Parsing:
                 raise SyntaxErrors ("Extra characters is phrase")
         return cR
 
-class T:
-    def read(self,*args,**kw):
-        NotImplementedError
-class V(T): pass
-class Var(V):
-    def __init__(self,var):
-        self.var = var
-    def __repr__(self):
-        return "({}:Var)".format(repr(self.var))
-class Con(V):
-    def __init__(self,string,alst):
-        self.string = string
-        self.alst = alst
-    def __repr__(self):
-        return "( {} {s} {} ):Con".format(s=self.string,*self.alst)
-p = Parsing(a) # a = functor_Lexical (LamKey)
-def makeFun(t1,t2):
-    return Con("->",[t1,t2])
-def typ(toks):
-    # 优先级低的在外 这个类似 Parsec
-    #print "typ:",toks
-    left =  p.bind( p.seq(atom,p._seq("->",atom)) , lambda x:makeFun(x[0],x[1]) )
-    tmp = p.Or(left,atom)
-    return tmp(toks)
-def atom(toks):
-    #print "atom:",toks
-    quote =  p.bind( p.seq( p._a("'") ,p.id),lambda x:Var(''.join(x)) )
-    # Var o op^ => f o g x = f (g x) => Var (op^ x)
-    #bracket = p.bind ( p.seq(p._seq("(",typ),p._a(")")) ,lambda x:x[0])
-    bracket = p.bind(p.seq(p._seq("(",typ),p._a(")")),lambda x:x[0])
-    return p.Or(quote,bracket)(toks)
-def read(toks):
-    return p.reader(typ)(toks)
-#print (a.scan("'abc"))
-#print p.Or(p._a("'"),p.id)(a.scan("abc"))
-#print p.repeat(p._a("'"))(a.scan("'''"))
-#print p._seq("(",p.id)(a.scan("(abc"))
-#print p.bind( p.seq(p.id,p._seq("->",p.id)) , lambda x:makeFun(x[0],x[1]) )(a.scan("abc -> abc"))
-#print p.bind(p.seq(p._seq("(",p.id),p._a(")")),lambda x:x[0])(a.scan("(abc)"))
-print read(" 'a -> ('b -> 'c) ")
-print read(" ('a -> 'b) -> 'c ")
+def test2():
+    # datatype Expr = Var of string
+    #               | Lam of string * Expr
+    #               | App of Expr * Expr
+    class Expr: pass
+    class ExprValue(Expr): pass
+    class Var(ExprValue):
+        def __init__(self,s):
+            self.s = s # s:string
+        def __repr__(self): 
+            return "{}".format(self.s)
+    class Lam(ExprValue):
+        def __init__(self,argv,body):
+            self.argv = argv # argv : string
+            self.body = body # body : Expr
+        def __repr__(self):
+            return "\\ {} -> {}".format(self.argv,self.body)
+    class App(ExprValue):
+        def __init__(self,t1,t2):
+            # t1 : Expr ,t2 : Expr
+            self.t1 = t1
+            self.t2 = t2
+        def __repr__(self):
+            return "( {}  {} )".format(self.t1,self.t2)
+    class Key(KeyWord): 
+        # 定义保留关键字 
+        @classmethod
+        def alphas(self):
+            # 定义保留 词
+            return ['it' ]
+        @classmethod
+        def symbols(self):
+            # 定义保留符号
+            return ['_',"\\","->","(",")"]
+    # var = abc
+    # Lam = \ a . t
+    # App = (t1 t2)
+    mlam = functor_Lexical(Key)
+    p = Parsing(mlam)
+    def atom(toks):
+        return p.Or(p.id,p.key("_"))(toks)
+    def var(toks):
+        return p.bind(atom,lambda x: p.mreturn(Var(x) ))(toks)
+    def lam(toks):
+        tmp = p.bind( p.key("\\"),
+                      lambda _ : p.bind(atom,
+                    lambda argv : p.bind(p.key("->"),
+                        lambda _ : p.bind(expr,
+                        lambda body: p.mreturn( Lam(argv,body) ) ))))
+        return tmp(toks)
+
+    def app(toks):
+        tmp = p.bind(p.key("("),
+                     lambda _ : p.bind(expr,
+                        lambda t1 : p.bind (expr,
+                        lambda t2 : p.bind (p.key(")"),
+                                       lambda _ : p.mreturn ( App(t1,t2) )))))
+                                  
+        return tmp(toks)
+    def expr(toks):
+        return p.Or(p.Or(var,p.Or(lam,app)),p.empty)(toks)
+    def read(string):
+        return p.reader(expr)(string)
+    print var(mlam.scan(" abc "))
+    print p.reader(var)("abc")
+    print lam(mlam.scan(" \\a->b "))
+    print p.reader(lam)("\\abc->abc")
+    print p.reader(app)(" (a b) ")
+    print p.reader(expr)(" (a b) ")
+    print p.reader(expr)("\\abc->(abc c)")
+    print p.reader(expr)("(abc \\a->a)")
+    print p.reader(expr)("(\\a->a b)")
+    print p.reader(expr)("(\\a->(a a) b)")
+    print p.reader(expr)(" ( (a b) ( \\ a -> a b)) ")
+    while 1:
+        try:
+            tmp = mlam.scan(raw_input("\n>> "))
+            print tmp
+            print "=>",expr(tmp)
+        except Exception,e:
+            print e
+            continue
+if __name__ == '__main__':
+    #test1()
+    test2()
